605786a665c6696d01c315dd6f7f91bcf152afeb
Matúš Jakuboc <xjakuboc@fi.muni.cz>
2024-04-04 21:18:03 +0200

    INIT

---
+++ src/main/java/com/jakuboc/TenisCourtSystem/TenisCourtSystemApplication.java
@@ -0,0 +1,13 @@
+package com.jakuboc.TenisCourtSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TenisCourtSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TenisCourtSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jakuboc/TenisCourtSystem/TenisCourtSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jakuboc.TenisCourtSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TenisCourtSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
