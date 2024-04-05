bff4c7b453687402b36773f940b15f0814e20e61
Matúš Jakuboc <xjakuboc@fi.muni.cz>
2024-04-05 16:34:28 +0200

    FIX findAll query to generic class

--- src/main/java/com/jakuboc/TennisCourtSystem/repositories/Impl/GenericCrudRepositoryImpl.java
+++ src/main/java/com/jakuboc/TennisCourtSystem/repositories/Impl/GenericCrudRepositoryImpl.java
@@ -1,6 +1,5 @@
 package com.jakuboc.TennisCourtSystem.repositories.Impl;
 
-import com.jakuboc.TennisCourtSystem.domain.entities.User;
 import com.jakuboc.TennisCourtSystem.repositories.GenericCrudRepository;
 import jakarta.transaction.Transactional;
 import org.hibernate.Session;
@@ -13,7 +12,6 @@ import java.util.Optional;
 public abstract class GenericCrudRepositoryImpl<T, ID> implements GenericCrudRepository<T, ID> {
     private final SessionFactory sessionFactory;
     private final Class<T> clazz;
-
 
     protected GenericCrudRepositoryImpl(SessionFactory sessionFactory, Class<T> clazz) {
         this.sessionFactory = sessionFactory;
@@ -30,8 +28,7 @@ public abstract class GenericCrudRepositoryImpl<T, ID> implements GenericCrudRep
     @Override
     @Transactional
     public List<T> findAll() {
-        // TODO change later
-        return (List<T>) getSession().createQuery( "select e from User e", User.class).list();
+        return getSession().createQuery( "select e from " + clazz.getSimpleName() + " e", clazz).list();
     }
 
     @Override
